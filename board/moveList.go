362023add9d6429ec552ee1280614ef23d29ece6
nchandur <chandur.nischal@gmail.com>
2026-03-07 15:37:19 -0500

    feat: added move generation for pawns

--- board/moveList.go
+++ board/moveList.go
@@ -18,44 +18,140 @@ func NewMoveList() MoveList {
 	return ml
 }
 
-func (m *MoveList) AddQuietMove(board *Board, move int) {
+func (m *MoveList) addQuietMove(move int) {
 	m.moves[m.count].move = move
 	m.moves[m.count].score = 0
 	m.count++
 }
 
-func (m *MoveList) AddCaptureMove(board *Board, move int) {
+func (m *MoveList) addCaptureMove(move int) {
 	m.moves[m.count].move = move
 	m.moves[m.count].score = 0
 	m.count++
 }
 
-func (m *MoveList) AddEnPassantMove(board *Board, move int) {
+func (m *MoveList) addEnPassantMove(move int) {
 	m.moves[m.count].move = move
 	m.moves[m.count].score = 0
 	m.count++
 }
 
-func (m *MoveList) AddWhitePawnCaptureMove(board *Board, from, to Square, capture Piece) {
+func (m *MoveList) addWhitePawnCaptureMove(from, to Square, capture Piece) {
 	if Fr120ToRank(int(from)) == Seven {
-		m.AddCaptureMove(board, NewMove(from, to, capture, wQ, false, false).move)
-		m.AddCaptureMove(board, NewMove(from, to, capture, wR, false, false).move)
-		m.AddCaptureMove(board, NewMove(from, to, capture, wB, false, false).move)
-		m.AddCaptureMove(board, NewMove(from, to, capture, wK, false, false).move)
+		m.addCaptureMove(NewMove(from, to, capture, wQ, false, false).move)
+		m.addCaptureMove(NewMove(from, to, capture, wR, false, false).move)
+		m.addCaptureMove(NewMove(from, to, capture, wB, false, false).move)
+		m.addCaptureMove(NewMove(from, to, capture, wK, false, false).move)
 	} else {
-		m.AddCaptureMove(board, NewMove(from, to, capture, Empty, false, false).move)
+		m.addCaptureMove(NewMove(from, to, capture, Empty, false, false).move)
 	}
 }
 
-func (m *MoveList) AddWhitePawnMove(board *Board, from, to Square) {
+func (m *MoveList) addWhitePawnMove(from, to Square) {
 	if Fr120ToRank(int(from)) == Seven {
-		m.AddQuietMove(board, NewMove(from, to, Empty, wQ, false, false).move)
-		m.AddQuietMove(board, NewMove(from, to, Empty, wR, false, false).move)
-		m.AddQuietMove(board, NewMove(from, to, Empty, wB, false, false).move)
-		m.AddQuietMove(board, NewMove(from, to, Empty, wK, false, false).move)
+		m.addQuietMove(NewMove(from, to, Empty, wQ, false, false).move)
+		m.addQuietMove(NewMove(from, to, Empty, wR, false, false).move)
+		m.addQuietMove(NewMove(from, to, Empty, wB, false, false).move)
+		m.addQuietMove(NewMove(from, to, Empty, wK, false, false).move)
 	} else {
-		m.AddQuietMove(board, NewMove(from, to, Empty, Empty, false, false).move)
+		m.addQuietMove(NewMove(from, to, Empty, Empty, false, false).move)
 	}
+}
+
+func (m *MoveList) addBlackPawnCaptureMove(from, to Square, capture Piece) {
+	if Fr120ToRank(int(from)) == Two {
+		m.addCaptureMove(NewMove(from, to, capture, bQ, false, false).move)
+		m.addCaptureMove(NewMove(from, to, capture, bR, false, false).move)
+		m.addCaptureMove(NewMove(from, to, capture, bB, false, false).move)
+		m.addCaptureMove(NewMove(from, to, capture, bK, false, false).move)
+	} else {
+		m.addCaptureMove(NewMove(from, to, capture, Empty, false, false).move)
+	}
+}
+
+func (m *MoveList) addBlackPawnMove(from, to Square) {
+	if Fr120ToRank(int(from)) == Two {
+		m.addQuietMove(NewMove(from, to, Empty, bQ, false, false).move)
+		m.addQuietMove(NewMove(from, to, Empty, bR, false, false).move)
+		m.addQuietMove(NewMove(from, to, Empty, bB, false, false).move)
+		m.addQuietMove(NewMove(from, to, Empty, bK, false, false).move)
+	} else {
+		m.addQuietMove(NewMove(from, to, Empty, Empty, false, false).move)
+	}
+}
+
+func (m *MoveList) pawnMoveGeneration(board *Board) {
+	m.count = 0
+	side := board.SideToMove
+
+	switch side {
+	case White:
+		for pieceNum := range board.PieceNumber[wP] {
+
+			sq := Square(board.PieceList[wP][pieceNum])
+
+			if board.Pieces[sq+10] == Empty {
+				m.addWhitePawnMove(sq, sq+10)
+				if Fr120ToRank(int(sq)) == Two && board.Pieces[sq+20] == Empty {
+					m.addQuietMove(NewMove(sq, sq+20, Empty, Empty, false, true).move)
+				}
+
+			}
+
+			tempSq := board.Pieces[sq+9]
+			if tempSq != 120 && pieceColor[tempSq] == Black {
+				m.addWhitePawnCaptureMove(sq, sq+9, board.Pieces[sq+9])
+			}
+
+			tempSq = board.Pieces[sq+11]
+			if tempSq != 120 && pieceColor[tempSq] == Black {
+				m.addWhitePawnCaptureMove(sq, sq+11, board.Pieces[sq+11])
+			}
+
+			if (sq + 9) == board.EnPass {
+				m.addCaptureMove(NewMove(sq, sq+9, Empty, Empty, true, false).move)
+			}
+
+			if (sq + 11) == board.EnPass {
+				m.addCaptureMove(NewMove(sq, sq+11, Empty, Empty, true, false).move)
+			}
+
+		}
+	case Black:
+		for pieceNum := range board.PieceNumber[bP] {
+
+			sq := Square(board.PieceList[bP][pieceNum])
+
+			if board.Pieces[sq-10] == Empty {
+				m.addBlackPawnMove(sq, sq-10)
+				if Fr120ToRank(int(sq)) == Seven && board.Pieces[sq-20] == Empty {
+					m.addQuietMove(NewMove(sq, sq-20, Empty, Empty, false, true).move)
+				}
+
+			}
+
+			tempSq := board.Pieces[sq-9]
+			if tempSq != 120 && pieceColor[tempSq] == White {
+				m.addBlackPawnCaptureMove(sq, sq-9, board.Pieces[sq-9])
+			}
+
+			tempSq = board.Pieces[sq-11]
+			if tempSq != 120 && pieceColor[tempSq] == White {
+				m.addBlackPawnCaptureMove(sq, sq-11, board.Pieces[sq-11])
+			}
+
+			if (sq - 9) == board.EnPass {
+				m.addCaptureMove(NewMove(sq, sq-9, Empty, Empty, true, false).move)
+			}
+
+			if (sq - 11) == board.EnPass {
+				m.addCaptureMove(NewMove(sq, sq-11, Empty, Empty, true, false).move)
+			}
+
+		}
+
+	}
+
 }
 
 func (m *MoveList) Generate(board *Board) {
@@ -64,58 +160,18 @@ func (m *MoveList) Generate(board *Board) {
 		return
 	}
 
-	m.count = 0
-	side := board.SideToMove
-
-	if side == White {
-		for pieceNum := range board.PieceNumber[wP] {
-
-			sq := Square(board.PieceList[wP][pieceNum])
-
-			if board.Pieces[sq+10] == Empty {
-				m.AddWhitePawnMove(board, sq, sq+10)
-				if Fr120ToRank(int(sq)) == Two && board.Pieces[sq+20] == Empty {
-					m.AddQuietMove(board, NewMove(sq, sq+20, Empty, Empty, false, true).move)
-				}
-
-			}
-
-			tempSq := board.Pieces[sq+9]
-			if tempSq != 120 && pieceColor[tempSq] == Black {
-				m.AddWhitePawnCaptureMove(board, sq, sq+9, board.Pieces[sq+9])
-			}
-
-			tempSq = board.Pieces[sq+11]
-			if tempSq != 120 && pieceColor[tempSq] == Black {
-				m.AddWhitePawnCaptureMove(board, sq, sq+11, board.Pieces[sq+11])
-			}
-
-			if (sq + 9) == board.EnPass {
-				m.AddCaptureMove(board, NewMove(sq, sq+9, Empty, Empty, true, false).move)
-			}
-
-			if (sq + 11) == board.EnPass {
-				m.AddCaptureMove(board, NewMove(sq, sq+11, Empty, Empty, true, false).move)
-			}
-
-		}
-	}
-
-	if side == Black {
-
-	}
+	m.pawnMoveGeneration(board)
 
 }
 
 func (m *MoveList) String() string {
 	var builder strings.Builder
 
-	fmt.Fprintf(&builder, "Move List: %d\n", m.count)
-
 	for idx := range m.count {
 		score := m.moves[idx].score
-		fmt.Fprintf(&builder, "Move #%d: %s Promoted: %d\t(score: %d)\n", idx+1, m.moves[idx].String(), m.moves[idx].PromotedPiece(), score)
+		fmt.Fprintf(&builder, "Move #%d: %s (score: %d)\n", idx+1, m.moves[idx].String(), score)
 	}
 
+	fmt.Fprintf(&builder, "Move List: %d\n", m.count)
 	return builder.String()
 }
