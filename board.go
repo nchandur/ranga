7f017ff1b58e9fa43c95b07ee1a8ee9b2dad3144
nchandur <chandur.nischal@gmail.com>
2026-03-10 19:20:10 -0400

    feat: added fen parser

--- board.go
+++ board.go
@@ -1,21 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 const (
-	BOARD_SQUARE_NUM = 120
+	BOARD_SQUARE_NUM   = 120
+	MAX_GAME_MOVES     = 4096
+	MAX_POSITION_MOVES = 256
+	MAX_DEPTH          = 64
+)
+
+const (
+	START     = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+	PieceChar = ".PNBRQKpnbqrk"
+	SideChar  = "wb-"
+	RankChar  = "12345678"
+	FileChar  = "abcdefgh"
 )
 
 type Board struct {
-	Pieces      []Piece // represents pieces on the 120-index board. the square on which a piece exists has its corresponding value
-	SideToMove  Color
-	FiftyMove   int
-	Ply         int
-	HistoryPly  int
-	Castling    CastleBit
-	Material    []int
-	PieceNumber []int // number of each piece on board
-	PieceList   []Square
-	EnPassant   Square
-	Hash        uint64 // unique value for a given configuration on board
+	Pieces        []Piece // represents pieces on the 120-index board. the square on which a piece exists has its corresponding value
+	SideToMove    Color
+	FiftyMove     int
+	Ply           int
+	HistoryPly    int
+	Castling      CastleBit
+	Material      []int
+	PieceNumber   []int // number of each piece on board
+	PieceList     []Square
+	EnPassant     Square
+	Hash          uint64 // unique value for a given configuration on board
+	MoveList      []int
+	MoveScores    []int
+	MoveListStart []int
 }
 
 func NewBoard() Board {
@@ -24,10 +43,14 @@ func NewBoard() Board {
 	board.Pieces = make([]Piece, BOARD_SQUARE_NUM)
 	board.PieceNumber = make([]int, 13)
 	board.PieceList = make([]Square, 130)
-	board.SideToMove = White
+	board.SideToMove = Both
 	board.Material = make([]int, 2)
 	board.EnPassant = NoSquare
 	board.Hash = 0
+
+	board.MoveList = make([]int, MAX_DEPTH*MAX_POSITION_MOVES)
+	board.MoveScores = make([]int, MAX_DEPTH*MAX_POSITION_MOVES)
+	board.MoveListStart = make([]int, MAX_DEPTH)
 
 	return board
 }
@@ -37,28 +60,127 @@ func (b *Board) PieceIdx(piece Piece) int {
 }
 
 func (b *Board) GenHash() uint64 {
-	var res uint64
 
-	piece := Empty
+	var key uint64
 
-	for sq := range BOARD_SQUARE_NUM {
-		piece = b.Pieces[sq]
+	// pieces
+	for sq := range 120 {
+		piece := b.Pieces[sq]
+		if piece != Empty && piece != 120 {
+			key ^= PieceKeys[piece][sq]
+		}
+	}
 
-		if piece != Empty && sq != int(Offboard) {
-			res ^= PieceKeys[(int(piece)*120)+sq]
+	// side to move
+	if b.SideToMove == White {
+		key ^= SideKey
+	}
+
+	// en passant
+	if b.EnPassant != NoSquare {
+		key ^= EnPassKeys[b.EnPassant]
+	}
+
+	// castling rights
+	key ^= CastleKeys[b.Castling]
+
+	return key
+
+}
+func (b *Board) Reset() {
+
+	for idx := range BOARD_SQUARE_NUM {
+		b.Pieces[idx] = 120
+	}
+
+	for idx := range 64 {
+		b.Pieces[Fr64To120[idx]] = Empty
+	}
+
+	for idx := range b.PieceList {
+		b.PieceList[idx] = Square(Empty)
+	}
+
+	for idx := range b.Material {
+		b.Material[idx] = 0
+	}
+
+	for idx := range b.PieceNumber {
+		b.PieceNumber[idx] = 0
+	}
+
+	b.SideToMove = Both
+	b.EnPassant = NoSquare
+	b.FiftyMove = 0
+	b.HistoryPly = 0
+	b.Ply = 0
+	b.Castling = 0
+	b.Hash = 0
+	b.MoveList[b.Ply] = 0
+
+}
+
+func (b *Board) String() string {
+	var builder strings.Builder
+
+	builder.WriteString("\nGame Board:\n\n")
+
+	for rank := Rank8; rank >= Rank1; rank-- {
+		fmt.Fprintf(&builder, "%d  ", rank+1)
+
+		for file := FileA; file <= FileH; file++ {
+			sq := FR2Square(file, rank)
+			piece := b.Pieces[sq]
+
+			fmt.Fprintf(&builder, "%c ", PieceChar[piece])
 		}
 
+		builder.WriteString("\n")
 	}
 
-	if b.SideToMove == White {
-		res ^= SideKey
+	builder.WriteString("\n   a b c d e f g h\n\n")
+
+	sideStr := ""
+
+	switch b.SideToMove {
+	case White:
+		sideStr = "W"
+	case Black:
+		sideStr = "B"
 	}
 
-	if b.EnPassant != NoSquare {
-		res ^= PieceKeys[b.EnPassant]
+	fmt.Fprintf(&builder, "Side To Move: %s\n", sideStr)
+
+	enPassStr := ""
+
+	if b.EnPassant == NoSquare {
+		enPassStr = "-"
+	} else {
+		enPassStr = fmt.Sprintf("%c%d", 'a'+FilesBoard[b.EnPassant], RanksBoard[b.EnPassant]+1)
 	}
 
-	res ^= CastleKeys[b.Castling]
+	fmt.Fprintf(&builder, "Enpassant Square: %s\n", enPassStr)
 
-	return res
+	castleStr := ""
+
+	if b.Castling&WKSide != 0 {
+		castleStr += "K"
+	}
+
+	if b.Castling&WQSide != 0 {
+		castleStr += "Q"
+	}
+
+	if b.Castling&BKSide != 0 {
+		castleStr += "k"
+	}
+
+	if b.Castling&BQSide != 0 {
+		castleStr += "q"
+	}
+
+	fmt.Fprintf(&builder, "Castling: %s\n", castleStr)
+	fmt.Fprintf(&builder, "Hash: %x\n", b.Hash)
+
+	return builder.String()
 }
---
+++ fen.go
@@ -0,0 +1,109 @@
+package main
+
+func (b *Board) ParseFEN(fen string) {
+
+	b.Reset()
+
+	rank := Rank8
+	file := FileA
+	piece := Empty
+	sq120 := Square(0)
+
+	fenCount := 0
+	n := len(fen)
+
+	for rank >= Rank1 && fenCount < n {
+		count := 1
+
+		switch fen[fenCount] {
+		case 'p':
+			piece = bP
+		case 'n':
+			piece = bN
+		case 'b':
+			piece = bB
+		case 'r':
+			piece = bR
+		case 'q':
+			piece = bQ
+		case 'k':
+			piece = bK
+
+		case 'P':
+			piece = wP
+		case 'N':
+			piece = wN
+		case 'B':
+			piece = wB
+		case 'R':
+			piece = wR
+		case 'Q':
+			piece = wQ
+		case 'K':
+			piece = wK
+
+		case '1', '2', '3', '4', '5', '6', '7', '8':
+			piece = Empty
+			count = int(fen[fenCount] - '0')
+
+		case '/', ' ':
+			rank--
+			file = FileA
+			fenCount++
+			continue
+		}
+
+		for range count {
+			sq120 = FR2Square(file, rank)
+			b.Pieces[sq120] = piece
+			file++
+		}
+
+		fenCount++
+
+	}
+
+	switch fen[fenCount] {
+	case 'w':
+		b.SideToMove = White
+	case 'b':
+		b.SideToMove = Black
+	default:
+		b.SideToMove = Both
+	}
+
+	fenCount += 2
+
+	for range 4 {
+		if fen[fenCount] == ' ' {
+			break
+		}
+
+		switch fen[fenCount] {
+		case 'K':
+			b.Castling |= WKSide
+		case 'Q':
+			b.Castling |= WQSide
+		case 'k':
+			b.Castling |= BKSide
+		case 'q':
+			b.Castling |= BQSide
+		}
+
+		fenCount++
+
+	}
+
+	fenCount++
+
+	if fen[fenCount] == '-' {
+		b.EnPassant = NoSquare
+	} else {
+		file := File(fen[fenCount] - 'a')
+		rank := Rank(fen[fenCount+1] - '1')
+		b.EnPassant = FR2Square(file, rank)
+	}
+
+	b.Hash = b.GenHash()
+
+}
