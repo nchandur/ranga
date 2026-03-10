8781935e8db25c29cdcbca5441d58b645195e3d3
nchandur <chandur.nischal@gmail.com>
2026-03-09 22:08:29 -0400

    feat: added more fields to board struct

--- board.go
+++ board.go
@@ -5,21 +5,29 @@ const (
 )
 
 type Board struct {
-	Pieces     []Piece // represents pieces on the 120-index board. the square on which a piece exists has its corresponding value
-	SideToMove Color
-	FiftyMove  int
-	Ply        int
-	HistoryPly int
-	Castling   CastleBit
-	Material   []int
+	Pieces      []Piece // represents pieces on the 120-index board. the square on which a piece exists has its corresponding value
+	SideToMove  Color
+	FiftyMove   int
+	Ply         int
+	HistoryPly  int
+	Castling    CastleBit
+	Material    []int
+	PieceNumber []int // number of each piece on board
+	PieceList   []Square
 }
 
 func NewBoard() Board {
 	board := Board{}
 
 	board.Pieces = make([]Piece, BOARD_SQUARE_NUM)
+	board.PieceNumber = make([]int, 13)
+	board.PieceList = make([]Square, 140)
 	board.SideToMove = White
 	board.Material = make([]int, 2)
 
 	return board
+}
+
+func (b *Board) PieceIdx(piece Piece) int {
+	return (int(piece) * 10) + b.PieceNumber[int(piece)]
 }
