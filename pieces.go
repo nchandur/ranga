6846a8d27931f814d9864cb7259db874545191d0
nchandur <chandur.nischal@gmail.com>
2026-03-09 21:38:54 -0400

    feat: changed conditionals to array lookups for pieces

--- pieces.go
+++ pieces.go
@@ -19,79 +19,34 @@ const (
 )
 
 // returns true if piece is pawn
-func (p *Piece) isPawn() bool {
-	return *p == wP || *p == bP
-}
+var isPawn = []bool{false, true, false, false, false, false, false, true, false, false, false, false, false}
 
 // returns true if piece is knight
-func (p *Piece) isKnight() bool {
-	return *p == wN || *p == bN
-}
+var isKnight = []bool{false, false, true, false, false, false, false, false, true, false, false, false, false}
 
 // returns true if piece is bishop
-func (p *Piece) isBishop() bool {
-	return *p == wB || *p == bB
-}
+var isBishop = []bool{false, false, false, true, false, false, false, false, false, true, false, false, false}
 
 // returns true if piece is rook
-func (p *Piece) isRook() bool {
-	return *p == wR || *p == bR
-}
+var isRook = []bool{false, false, false, false, true, false, false, false, false, false, true, false, false}
 
 // returns true if piece is queen
-func (p *Piece) isQueen() bool {
-	return *p == wQ || *p == bQ
-}
+var isQueen = []bool{false, false, false, false, false, true, false, false, false, false, false, true, false}
 
 // returns true if piece is king
-func (p *Piece) isKing() bool {
-	return *p == wK || *p == bK
-}
+var isKing = []bool{false, false, false, false, false, false, true, false, false, false, false, false, true}
 
 // returns true the piece slides across the board
-func (p *Piece) isSlider() bool {
-	return *p == wB || *p == bB || *p == wR || *p == bR || *p == wQ || *p == bQ
-}
+var isSlider = []bool{false, false, false, true, true, true, false, false, false, true, true, true, false}
 
 // returns true if piece is major
-func (p *Piece) isMajor() bool {
-	return p.isQueen() || p.isRook()
-}
+var isMajor = []bool{false, false, false, false, true, true, false, false, false, false, true, true, false}
 
 // returns true if piece is minor
-func (p *Piece) isMinor() bool {
-	return p.isBishop() || p.isKnight()
-}
+var isMinor = []bool{false, false, true, true, false, false, false, false, true, true, false, false, false}
 
 // return material value of piece
-func (p *Piece) Value() int {
-	switch *p {
-	case wK, bK:
-		return 50000
-	case wQ, bQ:
-		return 1000
-	case wR, bR:
-		return 500
-	case wN, bN, wB, bB:
-		return 300
-	case wP, bP:
-		return 100
-
-	default:
-		return 0
-	}
-}
+var pieceValue = []int{0, 100, 300, 300, 500, 1000, 50000, 100, 300, 300, 500, 1000, 50000}
 
 // returns color of piece
-func (p *Piece) Color() Color {
-
-	if *p > 0 && *p < 7 {
-		return White
-	}
-
-	if *p >= 7 {
-		return Black
-	}
-
-	return Both
-}
+var pieceColor = []Color{Both, White, White, White, White, White, White, Black, Black, Black, Black, Black, Black}
