c7594875aafd2bbdacbb47bb00de72c286adcc87
Nischal <74138199+nchandur@users.noreply.github.com>
2026-09-02 20:50:39 -0400

    feat: psqt and mobility (#32)
    * feat: added and initialized eval masks
    
    * feat: added piece square evaluation
    
    * feat: split evaluation into hce
    
    * fix: double pawn count and rook mobility
    
    * fix: bumped dockefile base image
    
    * doc: updated changelog

--- internal/evaluate/evaluate.go
+++ internal/evaluate/evaluate.go
@@ -2,14 +2,6 @@ package evaluate
 
 import "ranga/internal/board"
 
-type Evaluator struct{}
-
-func (e *Evaluator) Evaluate(b *board.Board) int {
-	score := 0
-
-	for _, piece := range b.Mailbox {
-		score += board.PieceValue[piece]
-	}
-
-	return score
+type Evaluator interface {
+	Evaluate(b *board.Board) int
 }
