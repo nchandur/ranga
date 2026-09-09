2d7c41471ed2dcdf3ee172e65fa0eba55f470736
nchandur <chandur.nischal@gmail.com>
2026-09-08 23:06:45 -0400

    feat: added san parser

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
-	"os"
-	"ranga/internal/uci"
+	"fmt"
+	"log"
+	"ranga/internal/board"
+	"ranga/internal/pgn"
 )
 
 var version string = ""
 
 func main() {
 
-	engine := uci.NewEngine(os.Stdin, os.Stdout, version)
-	engine.Run()
+	b := board.NewBoard()
+
+	if err := b.ParseFEN("r3kbnr/1bqppppp/p1p5/8/4P3/1P1B4/P1P2PPP/RNBQK2R w KQkq - 1 8"); err != nil {
+		log.Fatal(err)
+	}
+
+	b.Print()
+
+	san := "O-O"
+
+	move := pgn.ParseSAN(&b, san)
+
+	fmt.Println("San: ", san, "-> Move: ", move.String())
+
+	fmt.Println(move.IsCastle())
+	fmt.Println(move.Source(), move.Target())
+
+	// engine := uci.NewEngine(os.Stdin, os.Stdout, version)
+	// engine.Run()
 
 }
