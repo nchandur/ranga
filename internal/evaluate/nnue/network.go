8802aba63e1d4a25c15a4bb9dbbafe3eae71d18a
nchandur <chandur.nischal@gmail.com>
2026-09-12 18:38:07 -0400

    feat: added function to load random weights

--- internal/evaluate/nnue/network.go
+++ internal/evaluate/nnue/network.go
@@ -1,10 +1,28 @@
 package nnue
 
+import "math/rand"
+
 type Network struct {
 	FeatureWeights [768]Accumulator
 	FeatureBias    Accumulator
 	OutputWeights  [2 * HiddenSize]int16
 	OutputBias     int16
+}
+
+// fills the network with small random weights: purely for wiring up search
+func (net *Network) Randomize() {
+	for f := range net.FeatureWeights {
+		for i := range HiddenSize {
+			net.FeatureWeights[f].Values[i] = int16(rand.Intn(201) - 100)
+		}
+	}
+	for i := range HiddenSize {
+		net.FeatureBias.Values[i] = int16(rand.Intn(201) - 100)
+	}
+	for i := range net.OutputWeights {
+		net.OutputWeights[i] = int16(rand.Intn(201) - 100)
+	}
+	net.OutputBias = int16(rand.Intn(201) - 100)
 }
 
 func (net *Network) Evaluate(us, them *Accumulator) int32 {
@@ -17,7 +35,7 @@ func (net *Network) Evaluate(us, them *Accumulator) int32 {
 
 	// not side to move accumulator
 	for i := range HiddenSize {
-		output += screlu(them.Values[i]) * int32(net.OutputWeights[i])
+		output += screlu(them.Values[i]) * int32(net.OutputWeights[HiddenSize+i])
 	}
 
 	// reduce quantization
